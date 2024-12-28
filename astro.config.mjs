5833b5760e6cd797c66f0943eb0c104ecd2be400
David Diaz <diazdavid.info@gmail.com>
2024-12-28 13:17:03 +0100

    add integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
+import tailwind from '@astrojs/tailwind';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: cloudflare(),
+  integrations: [tailwind(), sitemap()]
+});
